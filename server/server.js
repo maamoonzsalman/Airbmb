7efd7066f4e4fa582893cd1db7fb6bbee2f5692c
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2024-11-09 15:44:38 -0800

    changed cors settings

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'http://airbmbucket.s3-website.us-east-2.amazonaws.com',
     credentials: true,
 }));
 
