ef96a679f22e2863046f71ec00b9de7868d68b5d
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2024-11-10 15:07:39 -0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
     return (
         <div>
-            <h1>Backends Test</h1>
+            <h1>Backends Testt</h1>
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     );
