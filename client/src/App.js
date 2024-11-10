b573cd0a3ac117a74152469b747ef83107e94ea1
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2024-11-10 13:58:30 -0800

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ function App() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
+        console.log('this', process.env.NODE_ENV)
         api.get('/api/test')
             .then((res) => setData(res.data))
             .catch((err) => console.error(err));
@@ -12,7 +13,7 @@ function App() {
 
     return (
         <div>
-            <h1>Backend Test</h1>
+            <h1>Backendd Test</h1>
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     );
