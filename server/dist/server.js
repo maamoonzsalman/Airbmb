789cd7464c6e1639a8be9d2470ffe73a5df992e3
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2024-11-11 12:01:35 -0800

    setup typescript config

---
+++ server/dist/server.js
@@ -0,0 +1,44 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
+dotenv.config();
+const pgSession = connectPgSimple(session);
+const app = express();
+app.use(express.json());
+const allowedOrigin = process.env.NODE_ENV === 'production'
+    ? 'http://airbmbucket.s3-website.us-east-2.amazonaws.com'
+    : 'http://localhost:3000';
+const corsOptions = {
+    origin: allowedOrigin,
+    credentials: true,
+};
+app.use(cors(corsOptions));
+// TODO: Add security settings for production
+app.use(session({
+    store: new pgSession({
+        conString: process.env.DATABASE_URL,
+    }),
+    secret: process.env.SESSION_SECRET || 'your_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+        httpOnly: true,
+    },
+}));
+app.get('/api/test', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.json({ message: 'Backend is reachable!' });
+}));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
